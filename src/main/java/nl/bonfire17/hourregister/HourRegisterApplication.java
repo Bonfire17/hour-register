2ef56772a9d6add0af21edc8d88077a40f58844c
Bart Bongers <bartbongers1998@gmail.com>
2019-05-27 15:27:09 +0200

    First Commit

---
+++ src/main/java/nl/bonfire17/hourregister/HourRegisterApplication.java
@@ -0,0 +1,13 @@
+package nl.bonfire17.hourregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HourRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/bonfire17/hourregister/HourRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.bonfire17.hourregister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HourRegisterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
