27185e46ebc939ccb509f5fcdaf3ab952ddf757c
Bart Bongers <bartbongers1998@gmail.com>
2019-05-28 11:02:44 +0200

    Toevoegen van de Department Klassen

--- src/main/java/nl/bonfire17/hourregister/Models/Department.java
+++ src/main/java/nl/bonfire17/hourregister/Models/Department.java
@@ -1,4 +1,49 @@
 package nl.bonfire17.hourregister.Models;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Department {
+
+    public String id;
+
+    private String name, info;
+
+    private ArrayList<User> users;
+
+    public Department(String name, String info) {
+        this.name = name;
+        this.info = info;
+
+        id = UUID.randomUUID().toString();
+        users = new ArrayList<User>();
+    }
+
+    //Getters & Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
+
+    //End Getters & Setters
 }
