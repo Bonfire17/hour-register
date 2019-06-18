28de0b904fc9d90bc2cbfc007c2d80feac3fde6e
bblekkink <466872@student.saxion.nl>
2019-06-18 11:00:25 +0200

    save files

---
+++ src/main/java/nl/bonfire17/hourregister/controller/LoginController.java
@@ -0,0 +1,14 @@
+package nl.bonfire17.hourregister.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    public String login(){
+        return "index";
+    }
+
+}
+
