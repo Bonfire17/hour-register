05fecdc7e6c2fb45153e968936f2fc2812c6d592
bblekkink <466872@student.saxion.nl>
2019-06-20 16:09:08 +0200

    Updated HTML

--- src/main/java/nl/bonfire17/hourregister/controller/LoginController.java
+++ src/main/java/nl/bonfire17/hourregister/controller/LoginController.java
@@ -2,11 +2,13 @@ package nl.bonfire17.hourregister.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("header", "Inloggen");
         return "index";
     }
 
