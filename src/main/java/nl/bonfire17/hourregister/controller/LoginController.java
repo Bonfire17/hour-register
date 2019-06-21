3eb5c79483f1aaee7872b59b52dcec41eb5bdef9
bblekkink <466872@student.saxion.nl>
2019-06-21 21:37:25 +0200

    Updated HTML and css Added sessions Login/out function added

--- src/main/java/nl/bonfire17/hourregister/controller/LoginController.java
+++ src/main/java/nl/bonfire17/hourregister/controller/LoginController.java
@@ -1,16 +1,65 @@
 package nl.bonfire17.hourregister.controller;
 
 
+import nl.bonfire17.hourregister.data.DataProviderSingleton;
+import nl.bonfire17.hourregister.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 @Controller
+@RequestMapping
 public class LoginController {
-    public String login(Model model){
-        model.addAttribute("header", "Inloggen");
+
+    private ArrayList<User> users = DataProviderSingleton.getInstance().getUsers();
+
+    @GetMapping
+    public String login(HttpSession session) {
+        if (session.getAttribute("userId") != null){
+            return "redirect:/clocksystem";
+        }
         return "index";
     }
+
+    @PostMapping(path = "/logincheck")
+    public RedirectView loginCheck(@RequestParam(name = "username") String username,
+                                   @RequestParam(name = "password") String password,
+                                   HttpSession session) {
+
+        for (int i = 0; i < this.users.size(); i++) {
+            if (username.equals(this.users.get(i).getUsername()) && password.equals(this.users.get(i).getPassword())) {
+
+                session.setAttribute("userId", this.users.get(i).id);
+                session.setMaxInactiveInterval(300);
+
+                return new RedirectView("/clocksystem");
+            }
+        }
+        return new RedirectView("");
+    }
+
+    //Ends the session
+    @PostMapping(path = "/logout")
+    public RedirectView logout(HttpSession session) {
+
+        session.removeAttribute("userId");
+
+        return new RedirectView("");
+
+    }
+
+
 
 }
 
