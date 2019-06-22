f4fdfb479ee6d8ed2b91cbe72ecac69896246d11
bblekkink <466872@student.saxion.nl>
2019-06-22 20:40:37 +0200

    Added function to the user part

--- src/main/java/nl/bonfire17/hourregister/controller/ClockInOutController.java
+++ src/main/java/nl/bonfire17/hourregister/controller/ClockInOutController.java
@@ -1,18 +1,70 @@
 package nl.bonfire17.hourregister.controller;
 
+import nl.bonfire17.hourregister.data.DataProviderSingleton;
+import nl.bonfire17.hourregister.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/clocksystem")
 public class ClockInOutController {
 
+    private ArrayList<User> users = DataProviderSingleton.getInstance().getUsers();
 
     @GetMapping
-    public String defaultUserPage(Model model){
-        model.addAttribute("header", "Gebruiker Paneel");
-        return "user/clockin";
+    public String defaultUserPage(HttpSession  session){
+        if (session.getAttribute("userId") != null) {
+            for (int i = 0; i < this.users.size(); i++) {
+                if (session.getAttribute("userId").equals(this.users.get(i).id) && !this.users.get(i).isWorking()) {
+                    return "user/clockin";
+                } else if (session.getAttribute("userId").equals(this.users.get(i).id) && this.users.get(i).isWorking()) {
+                    return "user/clockout";
+                }
+            }
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping(path = "/clockin")
+    public RedirectView clockin(HttpSession session) {
+
+        for (int i = 0; i < this.users.size(); i++) {
+            if (session.getAttribute("userId").equals(this.users.get(i).id) && !this.users.get(i).isWorking()) {
+                this.users.get(i).clockIn();
+            }
+        }
+        return new RedirectView("/clocksystem");
+    }
+
+    @PostMapping(path = "/clockout")
+    public RedirectView clockout (HttpSession session, HttpServletResponse response, @RequestParam(name = "break-time", required = false) String breaktime) {
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+
+        for (int i = 0; i < this.users.size(); i++) {
+            if (session.getAttribute("userId").equals(this.users.get(i).id) && this.users.get(i).isWorking()) {
+
+                if (breaktime != null) {
+                    this.users.get(i).clockOut(LocalTime.parse(breaktime, timeFormatter));
+                    Cookie cookie = new Cookie("break-time", breaktime);
+                    response.addCookie(cookie);
+                } else {
+                    this.users.get(i).clockOut(LocalTime.parse("00:00", timeFormatter));
+                }
+            }
+        }
+        return new RedirectView("/clocksystem");
     }
 }
