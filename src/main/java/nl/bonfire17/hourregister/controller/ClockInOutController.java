2ee264a1c977918fbe895d06472b9b1d68c1e0c0
bblekkink <466872@student.saxion.nl>
2019-06-22 21:41:33 +0200

    Fixed breaktime not registering

--- src/main/java/nl/bonfire17/hourregister/controller/ClockInOutController.java
+++ src/main/java/nl/bonfire17/hourregister/controller/ClockInOutController.java
@@ -4,13 +4,11 @@ import nl.bonfire17.hourregister.data.DataProviderSingleton;
 import nl.bonfire17.hourregister.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.time.LocalTime;
@@ -24,12 +22,13 @@ public class ClockInOutController {
     private ArrayList<User> users = DataProviderSingleton.getInstance().getUsers();
 
     @GetMapping
-    public String defaultUserPage(HttpSession  session){
+    public String defaultUserPage(HttpSession  session, Model model, @CookieValue(value = "lastbreaktime", defaultValue = "00:00") String cookieValue){
         if (session.getAttribute("userId") != null) {
             for (int i = 0; i < this.users.size(); i++) {
                 if (session.getAttribute("userId").equals(this.users.get(i).id) && !this.users.get(i).isWorking()) {
                     return "user/clockin";
                 } else if (session.getAttribute("userId").equals(this.users.get(i).id) && this.users.get(i).isWorking()) {
+                    model.addAttribute("lastbreaktime", cookieValue);
                     return "user/clockout";
                 }
             }
@@ -49,7 +48,7 @@ public class ClockInOutController {
     }
 
     @PostMapping(path = "/clockout")
-    public RedirectView clockout (HttpSession session, HttpServletResponse response, @RequestParam(name = "break-time", required = false) String breaktime) {
+    public RedirectView clockout (HttpSession session, HttpServletResponse response, @RequestParam(name = "break", required = false) String breaktime) {
 
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
 
@@ -58,7 +57,7 @@ public class ClockInOutController {
 
                 if (breaktime != null) {
                     this.users.get(i).clockOut(LocalTime.parse(breaktime, timeFormatter));
-                    Cookie cookie = new Cookie("break-time", breaktime);
+                    Cookie cookie = new Cookie("lastbreaktime", breaktime);
                     response.addCookie(cookie);
                 } else {
                     this.users.get(i).clockOut(LocalTime.parse("00:00", timeFormatter));
